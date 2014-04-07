b701effbe51653345757113b263746c9fdaacbf4
Sasha Naumchev <anaumchev@gmail.com>
2014-04-07 17:52:18 -0400

    Events managing with local store

--- js/models/event.js
+++ js/models/event.js
@@ -1,5 +1,22 @@
 App.Event = DS.Model.extend({
-	name: DS.attr('string'),
-	user: DS.attr('string'),
+	type: DS.attr(),
+	date: DS.attr(),
+    time: DS.attr(),
+    place: DS.attr()
 });
 
+
+App.Event.FIXTURES = [{
+    id: 1,
+    type: 'Birthday',
+    date: '04/07/2014',
+    time: '20:23:43',
+    place: 'Pittsburgh'
+}, {
+    id: 2,
+    type: 'Sporting Event',
+    date: '04/07/2014',
+    time: '20:23:43',
+    place: 'New York'
+}
+];
