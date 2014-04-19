9f0ea398b546413550f99760a68226f1d9f80e17
Sasha Naumchev <anaumchev@gmail.com>
2014-04-18 21:43:58 -0400

    Managing generic events binded to users

--- js/controllers/events_create_controller.js
+++ js/controllers/events_create_controller.js
@@ -2,11 +2,14 @@
  * Created by Sasha on 4/7/2014.
  */
 App.EventsCreateController = Ember.ObjectController.extend({
+    needs: ['auth'],
+    authBinding: "controllers.auth",
     actions: {
         save: function(){
            // create a record and save it to the store
             var newEvent = this.store.createRecord('event', {type: this.get("type"), date: this.get("date"),
-                time: this.get("time"), place: this.get("place")});
+                time: this.get("time"), place: this.get("place"), user: this.get('auth.currentUser.id')});
+            console.log(newEvent);
             newEvent.save();
             // redirects to the user itself
             this.transitionToRoute('event', newEvent);
