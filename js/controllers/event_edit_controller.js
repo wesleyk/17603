fed535e6decfa891685606c09c0051c0fcf2b4ac
Sasha Naumchev <anaumchev@gmail.com>
2014-04-30 11:33:48 -0400

    Filtering events by remind field

--- js/controllers/event_edit_controller.js
+++ js/controllers/event_edit_controller.js
@@ -2,9 +2,12 @@
  * Created by Sasha on 4/7/2014.
  */
 App.EventEditController = Ember.ObjectController.extend({
+    needs: ['auth'],
+    authBinding: "controllers.auth",
     actions: {
         save: function(){
             var event = this.get('model');
+            event.set("user", this.get('auth.currentUser.id'));
             // this will tell Ember-Data to save/persist the new record
             event.save();
             // then transition to the current user
