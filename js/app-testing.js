b94878424cbc20c0526434f721637c50ad5b125e
Zachary Sweigart <zsweigart@gmail.com>
2014-05-02 11:02:39 -0400

    Added tests for incomplete, edit, and delete events

--- js/app-testing.js
+++ js/app-testing.js
@@ -139,7 +139,7 @@ test('Add Event Button Not Logged In', function() {
   });
 });
 
-test('Add Event Button Not Logged In', function() {
+test('Add Event Button Logged In', function() {
   App.reset();
   
   visit('/').then(function() {
@@ -160,6 +160,27 @@ test('Add Event Button Not Logged In', function() {
 });
 
 test('Add Event', function() {
+  App.reset();
+  
+  visit('/').then(function() {
+    fillIn("#email", "test@test.com");
+    fillIn("#password", "test");
+    click("#loginBtn");
+    andThen(function() {
+      click("#events-link").then(function() {
+        fillIn("#event-date", "2015-12-25");
+        // Times are 24-hour clock
+        fillIn("#event-time", "12:30");
+        fillIn("#event-place", "CMU");
+        click("#save-event").then(function() {
+            ok(find("2015-12-25"));
+        });
+      });
+    });
+  });
+});
+
+test('Add Different Event', function() {
   App.reset();
   
   visit('/').then(function() {
@@ -204,3 +225,85 @@ test('Add Event and Remind', function() {
     });
   });
 });
+
+test('Add Incomplete Event', function() {
+  App.reset();
+  
+  visit('/').then(function() {
+    fillIn("#email", "test@test.com");
+    fillIn("#password", "test");
+    click("#loginBtn");
+    andThen(function() {
+      click("#events-link").then(function() {
+        fillIn("#event-date", "2015-12-25");
+        // Times are 24-hour clock
+        click("#save-event").then(function() {
+            ok(find("Choose event type"));
+            ok(find("Event date"));
+            ok(find("Event start"));
+            ok(find("Event place"));
+            ok(find("Remind?"));
+            ok(find("Ok"));
+        });
+      });
+    });
+  });
+});
+
+test('Edit Event', function() {
+   App.reset();
+  
+  visit('/').then(function() {
+    fillIn("#email", "test@test.com");
+    fillIn("#password", "test");
+    click("#loginBtn");
+    andThen(function() {
+      click("#events-link").then(function() {
+        fillIn("#event-date", "2015-12-25");
+        // Times are 24-hour clock
+        fillIn("#event-time", "12:30");
+        fillIn("#event-place", "CMU");
+        click("#save-event").then(function() {
+          click("#2015-12-25").then(function() {
+		    click("#edit-btn").then(function() {
+			  ok(find("Choose event type"));
+              ok(find("Event date"));
+              ok(find("Event start"));
+              ok(find("Event place"));
+              ok(find("Remind?"));
+              ok(find("Ok"));
+			});
+	      });
+        });
+      });
+    });
+  });
+});
+
+test('Delete Event', function() {
+  App.reset();
+  
+  visit('/').then(function() {
+    fillIn("#email", "test@test.com");
+    fillIn("#password", "test");
+    click("#loginBtn");
+    andThen(function() {
+      click("#events-link").then(function() {
+        fillIn("#event-date", "2015-12-25");
+        // Times are 24-hour clock
+        fillIn("#event-time", "12:30");
+        fillIn("#event-place", "CMU");
+        click("#save-event").then(function() {
+          click("#2015-12-25").then(function() {
+		    click("#delete-btn").then(function() {
+			  click("#confirm-delete-btn").then(function() {
+			    ok(!find("#2015-12-25");
+			  });
+			});
+	      });
+        });
+      });
+    });
+  });
+});
+
