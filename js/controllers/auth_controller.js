7b25d370ab9b347923cc8aaff11a8d573a5b062c
Nikita Chepanov <nikita@chepanov.com>
2014-05-02 16:47:51 -0400

    .

--- js/controllers/auth_controller.js
+++ js/controllers/auth_controller.js
@@ -31,7 +31,6 @@ App.AuthController = Ember.Controller.extend({
           console.log(newUser);
         });
 
-//        self.transitionToRoute('calendar');
         self.transitionToRoute('events');
       } else {
         this.set('authed', false);
--- js/models/event.js
+++ js/models/event.js
@@ -1,9 +1,9 @@
 App.Event = DS.Model.extend({
-    type: DS.attr(''),
-    date: DS.attr(''),
-    time: DS.attr(''),
-    place: DS.attr(''),
-    user: DS.attr(''),
+	type: DS.attr(''),
+	date: DS.attr(''),
+	time: DS.attr(''),
+	place: DS.attr(''),
+	user: DS.attr(''),
 	remind: DS.attr('')
 });
 
